af5072fd15168a0fc6d8b25069a5f24dd87fbfc7
hyunjoo2 <ttldlguswn@gmail.com>
2022-05-06 17:19:51 +0900

    디버깅 수정

--- resource/js/common.js
+++ resource/js/common.js
@@ -8,8 +8,45 @@ $(function () {
    
    
 function gnbSubMenu(){
-	
+	var gnb = $(".gnb-menu");
+	var gnbChild = $(".gnb-menu > li");
+	var gnbTitle = gnb.find('a');
 
+	gnbChild.on({
+		mouseover: function(){
+			$(this).addClass('on')			
+		},
+		mouseleave: function(){
+			$(this).removeClass('on');
+		}
+	});
+	gnbTitle.mouseover(function(){
+		headerOpen();
+	});
+	$('.header').mouseleave(function(){
+		headerClose();
+		//$('body').css('overflow', 'initial')
+	});
+
+	function headerOpen() {  //Tweenmax를 이용한 헤더 메뉴 슬라이드 open
+		TweenMax.killAll();
+		TweenMax.to($(".header"), .5, {
+			height : 480,
+			ease : 'easeOutExpo'
+		});
+		$(".header").addClass("open");
+	}
+
+	function headerClose() {  //Tweenmax를 이용한 헤더 메뉴 슬라이드 close
+		TweenMax.killAll();
+		TweenMax.to($(".header"), .5, {
+			height : 100,
+			ease : 'easeOutExpo',
+			onComplete : function() {
+				$(".header").removeClass("open");
+			}
+		});
+	}
 }
 
 function sitemap(){
